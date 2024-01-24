4abcd3c575369cbb451ec51254b27e1b7df2dc41
Marcus-devfs <marcusvini6277@gmail.com>
2024-01-23 21:57:46 -0300

    ajuste produtos route

--- src/helpers/configMenu.js
+++ src/helpers/configMenu.js
@@ -1,6 +1,6 @@
 export const menuItems = [
     { to: '/', text: 'HOME', icon: 'home_icon' },
     { to: '/ambients', text: 'AMBIENTES', icon: 'ambients_icon' },
-    { to: '/product/products', text: 'PRODUTOS', icon: 'produtos_icon' },
+    { to: '/product', text: 'PRODUTOS', icon: 'produtos_icon' },
     { to: '/contact/contacts', text: 'CONTATO', icon: 'tel-icon' },
  ];
